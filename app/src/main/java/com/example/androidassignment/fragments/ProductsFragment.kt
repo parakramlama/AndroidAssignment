997daa87e4255147d5571a2be34a73b786cf1dac
parakramlama <tmngparakram@gmail.com>
2021-08-09 19:59:13 +0545

    product fragments

--- app/src/main/java/com/example/androidassignment/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/androidassignment/fragments/ProductsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.androidassignment.fragments
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidassignment.R
 
 class ProductsFragment : Fragment() {
@@ -15,18 +19,80 @@ class ProductsFragment : Fragment() {
     }
 
     private lateinit var viewModel: ProductsViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ProductAdapter
+    private lateinit var btnCart: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.products_fragment, container, false)
+        val productDao = ProductDB.getInstance(requireContext()).getProductDAO()
+        val repository = ProductRepository(productDao)
+        val viewModelFactory = ProductViewModelFactory(repository)
+        productViewModel = ViewModelProvider(this,viewModelFactory).get(ProductViewModel::class.java)
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_products, container, false)
+        recyclerView = view.findViewById(R.id.productRecycler)
+        btnCart = view.findViewById(R.id.btnCart)
+
+        productViewModel.getAllProducts()
+
+        productViewModel.text.observe(viewLifecycleOwner, Observer { lstProducts ->
+            adapter.setList(lstProducts)
+        })
+
+
+        adapter = ProductAdapter(requireContext())
+        recyclerView.layoutManager= LinearLayoutManager(context)
+        recyclerView.adapter=adapter
+
+        btnCart.setOnClickListener{
+            startActivity(
+                Intent(
+                    context,
+                    CartActivity::class.java
+                )
+            )
+        }
+
+//        loadProducts()
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProductsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
+//    private fun loadProducts() {
+//        CoroutineScope(Dispatchers.IO).launch {
+//            try {
+//                val productRepository = ProductRepository()
+//                val response = productRepository.getAllProducts()
+//                if (response.success == true) {
+//                    val lstProducts = response.data
+//                    withContext(Dispatchers.Main) {
+//                        adapter =
+//                                ProductAdapter(lstProducts!!, requireContext())
+//                        recyclerView.layoutManager = LinearLayoutManager(context)
+//                        recyclerView.adapter = adapter
+//                    }
+//                }
+//            } catch (ex: Exception) {
+//                withContext(Dispatchers.Main) {
+//                    Toast.makeText(
+//                            context,
+//                            "Error : ${ex.toString()}", Toast.LENGTH_SHORT
+//                    ).show()
+//                }
+//            }
+//        }
+//    }
 }
+//        return inflater.inflate(R.layout.products_fragment, container, false)
+
+
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        viewModel = ViewModelProvider(this).get(ProductsViewModel::class.java)
+//        // TODO: Use the ViewModel
+//    }
+//
+//}
