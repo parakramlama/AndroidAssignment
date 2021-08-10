3f8d4c33188d0caf1dbc3929a1fe70cf13617106
parakramlama <tmngparakram@gmail.com>
2021-08-10 11:53:02 +0545

    single product repository

---
+++ app/src/main/java/com/example/androidassignment/repository/SingleProductRepository.kt
@@ -0,0 +1,16 @@
+package com.example.androidassignment.repository
+
+import com.example.androidassignment.api.MyApiRequest
+import com.example.androidassignment.api.ServiceBuilder
+import com.example.androidassignment.response.ProductResponse
+
+class SingleProductRepository : MyApiRequest() {
+    private val singleProductAPI = ServiceBuilder.buildService(SingleProductAPI::class.java)
+
+    //Display Single Product
+    suspend fun getSingleProduct(id:String): ProductResponse {
+        return apiRequest {
+            singleProductAPI.showSingleProduct(id)
+        }
+    }
+}
