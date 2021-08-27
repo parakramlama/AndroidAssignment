a0627146e034c3ccd421cc1d7a60984245813c34
parakramlama <tmngparakram@gmail.com>
2021-08-27 14:40:10 +0545

    entity

---
+++ wearables/src/main/java/com/parakram/wearables/entity/User.kt
@@ -0,0 +1,9 @@
+package com.parakram.wearables.entity
+
+data class User(
+    val _id : String? = null,
+    var user_username : String? =null,
+    var user_email : String? =null,
+    var user_contactno : String? = null,
+    var user_password : String? =null
+)
